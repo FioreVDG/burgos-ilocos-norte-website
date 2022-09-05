20d98ae44866fd391f786fcc67ac5d7e69bcb550
CMAlcarazLGUSuite <christianmartin.alcaraz@lgusuite.com>
2022-09-05 16:01:45 +0800

    update

--- src/app/landing-page-portal/about-us/geographic-info/geographic-info.component.ts
+++ src/app/landing-page-portal/about-us/geographic-info/geographic-info.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-geographic-info',
   templateUrl: './geographic-info.component.html',
-  styleUrls: ['./geographic-info.component.scss']
+  styleUrls: ['./geographic-info.component.scss'],
 })
 export class GeographicInfoComponent implements OnInit {
+  image1: string = '/assets/images/to-be-used/kapurpurawan-2.jpg';
+  image2: string = '/assets/images/to-be-used/pagali-natural-pool-2.jpeg';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/landing-page-portal/department/department-info-dialog/department-info-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-department-info-dialog',
+  templateUrl: './department-info-dialog.component.html',
+  styleUrls: ['./department-info-dialog.component.scss'],
+})
+export class DepartmentInfoDialogComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<DepartmentInfoDialogComponent>
+  ) {
+    console.log(data);
+  }
+
+  ngOnInit(): void {}
+}
